9ef509e02720a1a2afd3ee0ed2a9f114a4d21c9e
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-06-04 08:29:28 +0700

    entity generator

---
+++ src/main/java/com/example/demo/entities/CustomerEntity.java
@@ -0,0 +1,77 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer", schema = "shop_storage", catalog = "")
+public class CustomerEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "ID", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "Name", nullable = false, length = 100)
+    private String name;
+    @Basic
+    @Column(name = "PhoneNumber", nullable = false)
+    private int phoneNumber;
+    @Basic
+    @Column(name = "Address", nullable = true, length = 300)
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CustomerEntity that = (CustomerEntity) o;
+
+        if (id != that.id) return false;
+        if (phoneNumber != that.phoneNumber) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + phoneNumber;
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
