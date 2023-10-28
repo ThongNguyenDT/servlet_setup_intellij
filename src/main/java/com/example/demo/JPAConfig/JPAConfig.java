faf64e91345ad66b00093bbb7921068c4229acc5
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-10-29 00:27:10 +0700

    jpa from class

---
+++ src/main/java/com/example/demo/JPAConfig/JPAConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.JPAConfig;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAConfig {
+    public static EntityManager getEntityManager(){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+        return factory.createEntityManager();
+    }
+}
