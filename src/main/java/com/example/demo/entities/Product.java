1b4ab575101398f73279be4c3b62675768ec01a7
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-10-30 00:31:23 +0700

    my custom gen

--- src/main/java/com/example/demo/entities/Product.java
+++ src/main/java/com/example/demo/entities/Product.java
@@ -19,8 +19,48 @@ public class Product implements Serializable {
     @Basic
     @Column(name = "name", nullable = false, columnDefinition = "nvarchar(MAX)")
     private String name;
+    public Product() {
+        super();
+    }
 
 
+    public Integer getPhong() {
+        return phong;
+    }
+
+
+    public void setPhong(Integer phong) {
+        this.phong = phong;
+    }
+
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
     
 }
 
