7063d9a274d50bf5696a0e61c9a7691b7e2f514b
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-11-01 00:39:50 +0700

    template I$entity$

--- src/main/java/com/example/demo/DAO/ICategory.java
+++ src/main/java/com/example/demo/DAO/ICategory.java
@@ -1,6 +1,17 @@
 package com.example.demo.DAO;
 
 
-public class ICategory {
+import com.example.demo.entities.Category;
 
+import java.util.List;
+
+public interface ICategory {
+    void insert(Category category);
+    void update(Category category);
+    void delete(Integer dateid);
+    Category findById(Integer cateid);
+    List<Category> findAll();
+    List<Category> findByCategoryName(String cateName);
+    List<Category> findAll(Integer page, Integer pageSize);
+    Integer count();
 }
